c7e306a6ebdb56b522281d96045b3be1b1ebee3b
Nm1ss <91995566+Nm1ss@users.noreply.github.com>
2026-01-11 11:31:45 +0100

    added some modifcations

---
+++ make_gh_iso.sh
@@ -0,0 +1,1 @@
+OMARCHY_INSTALLER_REPO="Nm1ss/omarchy" OMARCHY_INSTALLER_REF="custom-mods" ./bin/omarchy-iso-make
---
+++ make_local_iso.sh
@@ -0,0 +1,2 @@
+# generate iso file from local omarchy repo
+OMARCHY_PATH="/home/voluspa/git/omarchy" OMARCHY_INSTALLER_REF="custom-mods" ./bin/omarchy-iso-make --local-source 
